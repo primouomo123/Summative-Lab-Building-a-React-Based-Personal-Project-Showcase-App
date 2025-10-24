6aa706413e2e591722a0a60e7596c3cdad64a534
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-24 07:18:01 -0400

    I added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import ProductContainer from "./pages/ProductContainer";
+import ProductList from "./pages/ProductList";
+import ProductCard from "./pages/ProductCard";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1>FlatShop</h1>
-        <p>Welcome to FlatShop, your one-stop shop for all things flat!</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductContainer />}>
+            <Route path="" element={<ProductList />} />
+            <Route path=":id" element={<ProductCard />} />
+            <Route path="new" element={<ProductForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
