2c57c6f15da3612a7d16a53c7ddd50f15614a767
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-24 07:27:57 -0400

    Home page is ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
-function Home() {}
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the FlatShop</h1>
+            <p>Explore our collection of amazing products.</p>
+        </div>
+    );
+}
 
 export default Home;
